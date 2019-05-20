f1aeb0f4e971e1b04fee81bc9454b0ec0497a215
daleksprinter <b1015120@fun.ac.jp>
2019-05-20 17:52:20 +0900

    add memo model

--- mymemoapp/models.py
+++ mymemoapp/models.py
@@ -3,3 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Memo(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+    )
+    text = models.TextField()
