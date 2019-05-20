436a653088d17bb9507ce792279d8e2f50d4c495
daleksprinter <b1015120@fun.ac.jp>
2019-05-20 18:40:38 +0900

    add text area for save

--- mymemoapp/views.py
+++ mymemoapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from mymemoapp.models import Memo
+from mymemoapp.models import Memo, User
 
 def index(request):
+    if request.method == 'POST':
+        txt = request.POST['text']
+        usr = User.objects.get(username = 'root')
+        memo = Memo.objects.create(author = usr, text = txt)
+        memo.save()
+
     memos = Memo.objects.all()
     return render(request, 'index.html', {"memos" : memos})
