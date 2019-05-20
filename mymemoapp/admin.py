ecae2c422a115ce8cfa7b6941993863b59652f75
daleksprinter <b1015120@fun.ac.jp>
2019-05-20 18:04:10 +0900

    add models to admin site

--- mymemoapp/admin.py
+++ mymemoapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from mymemoapp.models import Memo
+from mymemoapp.models import User
 # Register your models here.
+
+admin.site.register(Memo)
+admin.site.register(User)
--- mymemoapp/views.py
+++ mymemoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from mymemoapp.models import Memo
 
 def index(request):
-    return render(request, 'index.html', )
+    memos = Memo.objects.all()
+    return render(request, 'index.html', {"memos" : memos})
